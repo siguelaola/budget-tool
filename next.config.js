1f20c10d6d8321fb41dfd9f356692b2ad1aaae5a
Alejandro Portillo Guerrero <alejandro@siguelaola.com>
2023-05-04 18:34:55 +0200

    new

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
   images: {
     domains: ["images.unsplash.com", "www.example.com"],
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/budget-tool' : '',
   basePath: basePath,
   env: {
     SUPABASE_URL: process.env.SUPABASE_URL,
